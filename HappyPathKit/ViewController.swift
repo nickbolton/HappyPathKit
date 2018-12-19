7ae47ee193d2722dcdc54caea18efcc4a33b45b5
nickbolton <bolton.nick@gmail.com>
2018-12-19 10:41:59 -0600

    Initial Commit

---
+++ HappyPathKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappyPathKit
+//
+//  Created by Nick Bolton on 12/19/18.
+//  Copyright © 2018 Pixelbleed LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
